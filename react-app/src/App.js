bb89129a60761fff9d09e4bf6a4a15aac0d6e6e5
Your Name <jnguyengg@gmail.com>
2024-01-02 02:50:09 -0500

    created landing page and added new logos

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import logo from "../src/assets/taskify_logo.png";
+import homePoster from "../src/assets/taskify.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left-pane">
+        <div className="logo-container">
+          <img src={logo} className="logo" />
+        </div>
+        <div className="left-header">
+          <h1 className="header">Discover What You Can Achieve</h1>
+        </div>
+        <div className="left-text">
+          <p className="text">
+            Find out what tasks you need to accomplish, connect with others to
+            achieve these tasks, and learn new skills for a more productive and
+            interconnected journey.
+          </p>
+        </div>
+        <button className="get-started-btn">Get Started</button>
+      </div>
+
+      <div className="right-pane">
+        <img src={homePoster} className="home-poster" />
+      </div>
     </div>
   );
 }
